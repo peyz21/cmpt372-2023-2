9ab95a719cd1940cc222a2413c2d4c260484c9da
bobbychansfu <bobbychansfu@gmail.com>
2023-06-16 21:47:10 -0700

    mongo demo2

---
+++ topic4/4_mongodb/connect_mongo.js
@@ -0,0 +1,16 @@
+const uri = "mongodb+srv://root:root@cluster0.reg6e.mongodb.net/?retryWrites=true&w=majority"
+
+var MongoClient = require('mongodb').MongoClient;
+
+(async ()=>{
+    let client = await MongoClient.connect(uri, { useNewUrlParser:true, useUnifiedTopology:true})
+    let db = client.db('cmpt372')
+    const docs = db.collection("documents")
+    const res = await docs.updateOne({a:1},{$set: {n:"bobby"}},{upsert:true})
+    const cur = docs.find()
+    await cur.forEach(element => {
+        console.log(element)
+    })
+    client.close()
+    process.exit()
+})().catch(err => {console.error(err)})
---
+++ topic4/4_mongodb/mongoose_connect.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+mongoose.connect("mongodb+srv://root:root@cluster0.reg6e.mongodb.net/cmpt372?retryWrites=true&w=majority")
+
+var db = mongoose.connection
+
+db.on("error", console.error.bind(console, "error message"))
+
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    uname: {type: String},
+    age: {type: Number, min:[0,'not born yet!'], max:110},
+    password: {type: String, minlength:4}
+})
+
+var User = mongoose.model("user", userSchema)
+
+var creatUser = async() => {
+    var b = new User({uname:"steve",age:42,password:"45"})
+    try {
+        await b.save()
+        console.log("done!")
+    } catch (e) {
+        console.log(e.errors)
+    }
+    process.exit()
+}
+
+creatUser()
