6868af198b05be3c03b10614ce4b893bfd888535
PEYZ <zhiani2000@gmail.com>
2023-08-10 23:17:03 -0700

    test

--- topic3/mvc/app.js
+++ topic3/mvc/app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// this is the route that will be used to access the web app
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
