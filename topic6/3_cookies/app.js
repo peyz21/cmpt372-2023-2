efb6ba68b77b3737e248ef01646a5b2e667dc59b
PEYZ <zhiani2000@gmail.com>
2023-08-11 20:29:28 -0700

    test

---
+++ topic6/3_cookies/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// Use the cookie-parser middleware
+app.use(cookieParser());
+
+app.get("/setcookie", (req, res) => {
+  res.cookie("username", "JohnDoe", { maxAge: 900000, httpOnly: true });
+  res.send("Cookie has been set!");
+});
+
+app.get("/getcookie", (req, res) => {
+  const username = req.cookies["username"];
+  if (username) {
+    res.send(`Hello, ${username}!`);
+  } else {
+    res.send("No cookie found.");
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
